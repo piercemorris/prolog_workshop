9999e501f0e65135d0f8fd6e00e45b9131e5baec
Stephen Church <stephen.church@bjss.com>
2023-04-13 14:39:51 +0100

    Tidy up tax calculation code

--- Worksheet answers/tax_calculator.pl
+++ Worksheet answers/tax_calculator.pl
@@ -1,21 +1,31 @@
+%% TODO: Deal with final band that has no upper threshold
 calculateTax(LowerThreshold, UpperThreshold, PercentageTax, Value, Tax) :-
-    Value > UpperThreshold,
-    !,
-    TaxableAmount is UpperThreshold - LowerThreshold,
-    Tax is (TaxableAmount * PercentageTax)/100.
-
-calculateTax(LowerThreshold, UpperThreshold, PercentageTax, Value, Tax) :-
-    Value =< UpperThreshold,
     Value > LowerThreshold,
-    TaxableAmount is Value - LowerThreshold,
+    !,
+    UpperThresholdWithinBand is min(Value, UpperThreshold),
+    TaxableAmount is UpperThresholdWithinBand - LowerThreshold,
     Tax is (TaxableAmount * PercentageTax)/100.
 
-%% Does Prolog have data structures
+%% If the above rule fails, we want to record that the Tax is 0 as the band doesn't apply
+calculateTax(_, _, _, _, 0).
+
 %% No accumulator
-calculateTaxFromBands([], _, 0).
-calculateTaxFromBands([[LowerThreshold, UpperThreshold, PercentageTax]|RestOfBands], Value, TotalTax) :-
+calculateTax([], _, 0).
+calculateTax([[LowerThreshold, UpperThreshold, PercentageTax]|RestOfBands], Value, TotalTax) :-
     calculateTax(LowerThreshold, UpperThreshold, PercentageTax, Value, TaxForCurrentBand),
-    calculateTaxFromBands(RestOfBands, Value, TaxOfRestOfBands),
+    calculateTax(RestOfBands, Value, TaxOfRestOfBands),
     TotalTax is TaxForCurrentBand + TaxOfRestOfBands.
 
-%% With accumulator
+%% TODO: With accumulator
+calculateTaxAcc([], _, TotalTax, TotalTax).
+calculateTaxAcc([[LowerThreshold, UpperThreshold, PercentageTax]|RestOfBands], Value, AccTax, TotalTax) :-
+    calculateTax(LowerThreshold, UpperThreshold, PercentageTax, Value, TaxForCurrentBand),
+    NewAccTax is AccTax + TaxForCurrentBand,
+    calculateTaxAcc(RestOfBands, Value, NewAccTax, TotalTax).
+
+%% Wrappers
+calculateTax(Value, Tax) :-
+    calculateTax([[145000, 250000, 2], [250000, 325000, 5], [325000, 750000, 10], [750000, 1000000, 12]], Value, Tax).
+
+calculateTaxAcc(Value, Tax) :-
+    calculateTaxAcc([[145000, 250000, 2], [250000, 325000, 5], [325000, 750000, 10], [750000, 1000000, 12]], Value, 0, Tax).
